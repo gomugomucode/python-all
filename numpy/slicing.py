e65992cc3131068ee5726f1e4e478ab46ba5f601
gomugomucode <alinedtwins@gmail.com>
2026-06-01 20:13:44 +0545

    Add slicing example for numpy arrays

---
+++ numpy/slicing.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+array = np.array([[1,2,3,4],
+                  [5,6,7,8],
+                  [9,10,11,12],
+                  [13,14,15,16]])
+
+# Slicing the array  
+# Slicing syntax: array[start:stop:step]
+
+
+# printing the whole array using slicing
+print(array[:])  
