a7ca0699a3f39a70ed500b8836f927d02aa3e9d1
gomugomucode <alinedtwins@gmail.com>
2026-06-03 23:09:22 +0545

    Update comments for clarity in array slicing examples

--- numpy/slicing.py
+++ numpy/slicing.py
@@ -51,7 +51,22 @@ array = np.array([[1,2,3,4],
 
 # print(array[ : , -2]) # prints the second last column of the array
 
-print(array[ 1:3 , 2]) # prints the 3rd column of the 2nd and 3rd rows of the array
+# print(array[ 1:3 , 2]) # prints the 3rd column of the 2nd and 3rd rows of the array
 
 
 
+# row and colum slicing together
+
+# print(array[0 , 0]) # prints the element at 1st row and 1st column of the array
+
+# print(array[1,1]) # prints the element at 2nd row and 2nd column of the array
+
+# print(array[2,2]) # prints the element at 3rd row and 3rd column of the array
+
+# print(array[3,3]) # prints the element at 4th row and 4th column of the array
+
+# print(array[0:3, 0:3]) # prints the 1st, 2nd and 3rd rows and columns of the array
+
+# print(array[0:2 , 0:2]) # prints the 1st and 2nd rows and columns of the array
+
+print(array[0:3 :2 , 0:3 :2]) # prints the 1st and 3rd rows and columns of the array
