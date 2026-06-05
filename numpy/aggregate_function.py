560c8f723860427828947719ec3900de8e47924b
gomugomucode <alinedtwins@gmail.com>
2026-06-05 22:51:07 +0545

    Reorganize commented aggregate function examples for clarity

--- numpy/aggregate_function.py
+++ numpy/aggregate_function.py
@@ -9,7 +9,15 @@ print(arr1.shape)
 # print(np.sum(arr1))  # this will give us the sum of all the elements in the array
 # print(np.mean(arr1))  # this will give us the mean of all the elements in the array
 # print(np.min(arr1))  # this will give us the minimum value in the array
-# print(np.max(arr1))  # this will give us the maximum value in the array
-print(np.average(arr1))  # this will give us the average of all the elements in the array
-print(np.var(arr1))  # this will give us the variance of all the elements in the array
-print(np.std(arr1))  # this will give us the standard deviation of all the elements in the array
+# # print(np.max(arr1))  # this will give us the maximum value in the array
+# print(np.average(arr1))  # this will give us the average of all the elements in the array
+# print(np.var(arr1))  # this will give us the variance of all the elements in the array
+# print(np.std(arr1))  # this will give us the standard deviation of all the elements in the array
+
+# print(np.argmax(arr1))  # this will give us the index of the maximum value in the array
+# print(np.argmin(arr1))  # this will give us the index of the minimum value in the array
+
+# print(np.prod(arr1))  # this will give us the product of all the elements in the array
+
+# print(np.cumsum(arr1))  # this will give us the cumulative sum of all the elements in the array
+print(np.cumprod(arr1))  # this will give us the cumulative product of all the elements in the array
