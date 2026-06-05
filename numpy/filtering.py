f456a32be29d63606135c29f21c34d5ca9cde50f
gomugomucode <alinedtwins@gmail.com>
2026-06-05 23:33:28 +0545

    Implement age classification using np.select for improved clarity and structure

--- numpy/filtering.py
+++ numpy/filtering.py
@@ -21,5 +21,15 @@ ages  =  np.array([[22, 25, 18, 30, 28, 35, 40 , 27, 24 ,65,999],[32,98 ,65 , 45
 # adult = ages[ages >= 25]
 # print(adult)
 
-adult = np.where((ages >= 25 ) & (ages <60) , "Adult" , "Not Adult") 
-print(adult)
+#  we can use multiple condition in where function
+
+# people = np.where((ages >= 25 ) & (ages <60) , "Adult" , np.where(ages < 25 , "Teenager" , "Senior Citizen")) # in here the people variable will contain the value "Adult" for match the first condition and the unmatched value will be checked for the second condition if it match then it will be replaced by "Teenager" and if it does not match then it will be replaced by "Senior Citizen"
+
+# print(people)
+
+#  the anothe rway of doing this is by using the select function
+
+conditions = [ (ages < 25) , (ages >= 25) & (ages < 60) , (ages >= 60)]
+choices = ["Teenager" , "Adult" , "Senior Citizen"]
+people = np.select(conditions , choices , default="Unknown") # in here the people variable will contain the value "Teenager" for match the first condition and the unmatched value will be checked for the second condition if it match then it will be replaced by "Adult" and if it does not match then it will be replaced by "Senior Citizen"
+print(people)
