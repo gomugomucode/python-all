191a2f55416db1f9ef4cad76369df570e9775e92
gomugomucode <alinedtwins@gmail.com>
2026-06-05 23:44:00 +0545

    Add examples for generating 3D and 5D arrays of random floats using uniform() method

--- numpy/random_number.py
+++ numpy/random_number.py
@@ -20,10 +20,12 @@ rng = np.random.default_rng() # in this default_rng() is used to create a random
 # print(rng.random(size=5)) # This line generates an array of 5 random floats in the range [0.0, 1.0) using the random() method of the random number generator instance (rng) and prints it to the console. The size parameter specifies the number of random floats to generate.
 
 #  we can also specify thge high and low values for the random floats using the uniform() method
-print(rng.uniform(low=1.0, high=10.0, size=5)) # This line generates an array of 5 random floats in the range [1.0, 10.0) using the uniform() method of the random number generator instance (rng) and prints it to the console. The low and high parameters specify the range for the random float generation, and the size parameter specifies the number of random floats to generate.
+# print(rng.uniform(low=1.0, high=10.0, size=5)) # This line generates an array of 5 random floats in the range [1.0, 10.0) using the uniform() method of the random number generator instance (rng) and prints it to the console. The low and high parameters specify the range for the random float generation, and the size parameter specifies the number of random floats to generate.
 
 # #  if we want the random number in 2d array we can specify the size as a tuple
 # print(rng.uniform(low=1.0, high=10.0, size=(3, 4))) # This line generates a 2D array of random floats with 3 rows and 4 columns in the range [1.0, 10.0) using the uniform() method of the random number generator instance (rng) and prints it to the console. The low and high parameters specify the range for the random float generation, and the size parameter specifies the shape of the output array as a tuple (3, 4).
 
-# #  for 3d array
-print(rng.uniform(low=1.0, high=10.0, size=(2   , 3, 4))) # This line generates a 3D array of random floats with 2 blocks, 3 rows, and 4 columns in the range [1.0, 10.0) using the uniform() method of the random number generator instance (rng) and prints it to the console. The low and high parameters specify the range for the random float generation, and the size parameter specifies the shape of the output array as a tuple (2, 3, 4).
+# # #  for 3d array
+# print(rng.uniform(low=1.0, high=10.0, size=(2   , 3, 4))) # This line generates a 3D array of random floats with 2 blocks, 3 rows, and 4 columns in the range [1.0, 10.0) using the uniform() method of the random number generator instance (rng) and prints it to the console. The low and high parameters specify the range for the random float generation, and the size parameter specifies the shape of the output array as a tuple (2, 3, 4).
+
+print(rng.uniform(low=1.0, high=10.0, size=(5 ,5,5)))  # to generate the 5d array
