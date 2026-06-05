e9b8a70c98dc19e74d7f351df00b7cf45c69e72e
gomugomucode <alinedtwins@gmail.com>
2026-06-05 22:45:50 +0545

    Reformat comments for aggregate functions in NumPy example

--- numpy/aggregate_function.py
+++ numpy/aggregate_function.py
@@ -6,9 +6,10 @@ arr1 = np.array([[1,2,3,4,5,6,7,8,9],
 
 print(arr1.shape)
 
-print(np.sum(arr1))  # this will give us the sum of all the elements in the array
-print(np.mean(arr1))  # this will give us the mean of all the elements in the array
-print(np.min(arr1))  # this will give us the minimum value in the array
-print(np.max(arr1))  # this will give us the maximum value in the array
-
-
+# print(np.sum(arr1))  # this will give us the sum of all the elements in the array
+# print(np.mean(arr1))  # this will give us the mean of all the elements in the array
+# print(np.min(arr1))  # this will give us the minimum value in the array
+# print(np.max(arr1))  # this will give us the maximum value in the array
+print(np.average(arr1))  # this will give us the average of all the elements in the array
+print(np.var(arr1))  # this will give us the variance of all the elements in the array
+print(np.std(arr1))  # this will give us the standard deviation of all the elements in the array
