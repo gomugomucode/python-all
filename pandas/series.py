6401d8c9a4820e2bd9020a4a53732acf72e673ca
gomugomucode <alinedtwins@gmail.com>
2026-06-07 12:59:38 +0545

    feat: add examples and comments for creating and accessing pandas Series

---
+++ pandas/series.py
@@ -0,0 +1,33 @@
+
+# series is a one-dimensional labeled array capable of holding any data type (integers, strings, floating point numbers, Python objects, etc.). The axis labels are collectively referred to as the index. The basic method to create a Series is to call:
+
+import pandas as pd
+
+# s = pd.Series([1, 3, 5, 7, 9])
+# print(s)
+
+# we can also do this for above code snippet
+
+# name =["Tailoring / Sewing", "Handicrafts (knitting, beadwork, etc.)", "Embroidery Work", "Hand Embroidery", "Pote/Mala Making", "Cloth Bag Making", "Paper Bag Making", "Knitting and Crocheting", "Jewelry Making", "Traditional Broom Making", "Bamboo Crafting", "Traditional Sculpture", "Pottery", "Candle Making", "Soap Making", "Agarbatti Making", "Pickle Making and Selling", "Fish Pickle Making and Selling", "Homemade Snacks and Food Delivery", "Catering Service (small scale)", "Baking and Cake Decoration", "Jam, Jelly and Sauce Production", "Nepali Food Recipe Content Creation", "Vegetable Farming", "Mushroom Farming", "Indoor Plant Selling / Kitchen Garden Setup", "Organic Compost Production", "Content Writing", "Digital Marketing", "Web Development", "Graphic Design", "Photography", "Bookkeeping / Accounting", "Data Entry Operator", "Nepali Recipe Blog / YouTube Channel", "Affiliate Marketing / Blogging", "Online Course Creation", "Virtual Assistant Services", "Makeup Artist", "Beauty Parlor Service", "Event Planning"]
+
+# # creating a series from the list of names
+# s = pd.Series(name) #  here the padnas library is used to create a series from the list of names. The pd.Series() function takes the list as an argument and returns a Series object. The resulting Series will have an index starting from 0 and the values will be the names from the list.
+
+# print(s)
+
+age = [25, 30, 35, 40, 45]
+
+# series_age = pd.Series(age)
+
+# in the above code , we can also give index to the series
+series_age = pd.Series(age, index=['Alice', 'Bob', 'Charlie', 'David', 'Eve'])
+
+# print(series_age)
+
+# if we want to access the value of a specific index, we can do it like this
+
+# print(series_age['Alice'])
+
+# or we can use loc method to access the value of a specific position
+# print(series_age.loc['Alice'])  #loc method is used to access the value of a specific position using the index label of line 23 
+
