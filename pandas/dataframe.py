4ee97a8d0c4be568b0b8e73277bc87eef73eb5af
gomugomucode <alinedtwins@gmail.com>
2026-06-07 13:13:00 +0545

    feat: update DataFrame creation example with custom index and loc method usage

--- pandas/dataframe.py
+++ pandas/dataframe.py
@@ -6,6 +6,15 @@ import pandas as pd
 
 data = {'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'], 'Age': [25, 30, 35, 40, 45]}
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)  #by default it has index in numeric form starting from 0. We can also give index to the dataframe like this :
 
-print(df)
+df = pd.DataFrame(data, index=['a', 'b', 'c', 'd', 'e'])  # here the index is given as a list of strings ['a', 'b', 'c', 'd', 'e'] which will be used as the index for the DataFrame. 
+
+# if u need tho select single row from the dataframe using the custom index given to it then we use loc method do it like this
+# print(df.loc['a'])  #loc method is used to access the value of a specific position using the index label of line 23
+# print(df.loc['b'])
+# print(df.loc['c'])
+# print(df.loc['d'])
+# print(df.loc['e'])
+
+# print(df)
