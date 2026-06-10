fcd57ff4208669b061cbb190b0e67e61f0f22a01
gomugomucode <alinedtwins@gmail.com>
2026-06-10 13:34:24 +0545

    refactor: improve readability of Book class constructor by adding comments

--- 30dayspython/Library Book Catalogue.py
+++ 30dayspython/Library Book Catalogue.py
@@ -1,10 +1,11 @@
 class Book:
+    # Class variable to track the total count
     total_books = 0
-    def __init__(self , title, author, genre,  available = True):
-        self.title  = title
-        self.author  = author
-        self.genre  = genre
-        self.available  = available
-        Book.total_books += 1
 
-    
+    def __init__(self, title, author, genre , available = True):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.available  = available
+        # Increase the counter every time __init__ runs
+        Book.total_books += 1
