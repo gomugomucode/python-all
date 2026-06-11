aaad80f18b612adf90444d64c8ff01604143a5aa
gomugomucode <alinedtwins@gmail.com>
2026-06-11 15:57:55 +0545

    feat: add area method to Circle class for area calculation

--- 30dayspython/Shape Calculator.py
+++ 30dayspython/Shape Calculator.py
@@ -5,4 +5,8 @@ class Circle :
     def __init__(self ,radius ):
         self.radius  = radius 
 
- 
+    def  area(self):
+        area = math.pi * self.radius *self.radius 
+        return f"The area of circle is {area}"
+
+    
