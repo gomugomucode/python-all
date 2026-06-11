da7ff5565dd735d18483cf5c5b9462f9ff8373b0
gomugomucode <alinedtwins@gmail.com>
2026-06-11 16:06:35 +0545

    fix: improve exception handling for negative radius in area and perimeter methods

--- 30dayspython/Shape Calculator.py
+++ 30dayspython/Shape Calculator.py
@@ -6,10 +6,13 @@ class Circle :
         self.radius  = radius 
 
     def  area(self):
+        if self.radius <= 0:
+            raise ValueError(f"Radius cannot be negative. Got: {self.radius}") 
         area = math.pi * self.radius *self.radius 
         return f"The area of circle is {area}"
 
     def  perimeter(self):
+        if self.radius <= 0:
+            raise ValueError(f"Radius cannot be negative. Got: {self.radius}")
         perimeter = 2 * math.pi * self.radius  
         return f"The perimeter of circle is {perimeter}"
-        
