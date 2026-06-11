a4f31b68cd35f635994152ffd173396df6ff0f0c
gomugomucode <alinedtwins@gmail.com>
2026-06-11 18:20:39 +0545

    feat: initialize shapes_created in Circle and Rectangle classes

--- 30dayspython/Shape Calculator.py
+++ 30dayspython/Shape Calculator.py
@@ -1,9 +1,11 @@
 import math
 
 class Circle :
+    shapes_created = 0
 
     def __init__(self ,radius ):
         self.radius  = radius 
+        self.shapes_created += 1
 
     def  area(self):
         if self.radius <= 0:
@@ -16,19 +18,22 @@ class Circle :
             raise ValueError(f"Radius cannot be negative. Got: {self.radius}")
         perimeter = 2 * math.pi * self.radius  
         return f"The perimeter of circle is {perimeter}"
-    
+
+
 class Rectangle :
+    shapes_created = 0
 
     def __init__(self ,width , height  ):
         self.width   = width 
         self.height   = height
-    
+        self.shapes_created += 1
+
     def is_square(self):
         if self.width == self.height :
             return True
         else :
             return False
-    
+
     def  area(self):
         if self.width or self.height   <= 0:
             raise ValueError(f"Dimension  cannot be negative. Got: {self.radius}") 
@@ -44,13 +49,17 @@ class Rectangle :
     @staticmethod 
     def cm_to_inch(a):
         return(f"The value in inch is {a * 0.3937 }inch")
-    
-    @staticmethod
-    def inch_to_cm(a):
-        return(f"The value in cm is {a * 2.54 }cm")
 
-    @staticmethod 
+    
+     @staticmethod 
     def is_valid_dimension(val):
         if val < 0:
             return f"Dimension cannot be negative. Got: {val}"  
         return True 
+
+       
+        
+
+    
+        
+        
