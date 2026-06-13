100a53c47c54dd0b6254b7e613e0ada64cb98c3f
gomugomucode <alinedtwins@gmail.com>
2026-06-13 16:31:30 +0545

    feat: add selection.py for demonstrating DataFrame column selection; include comments and example code

---
+++ pandas/selection.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+# selection by column name
+#  syntax is :   selected_column = df['column_name']
+
+# print(df["Name"])
+
+# to print the full dataframe without truncation
+# pd.set_option('display.max_rows', None)
+# print(df["Name"])
+
+
+# print(df["Height"].to_string())
+# print(df["Weight"].to_string())
+# print(df["Type1"])
+# print(df["Type2"])
+
+
