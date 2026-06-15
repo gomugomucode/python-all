dccaa4de57f2c9d0cfa2d31e061949b025b1520b
gomugomucode <alinedtwins@gmail.com>
2026-06-15 16:04:32 +0545

    feat: implement BankAccount class with balance management methods

--- 30dayspython/Bank Management System.py
+++ 30dayspython/Bank Management System.py
@@ -1,13 +1,35 @@
-
-
 class BankAccount:
-    def __init__(self):
-        pass
+    def __init__(self, name, __balance):
 
-    def deposit(amount):
-        pass
+        self.name = name
+        self.__balance = __balance
 
-    def withdraw(amount):
-        pass
+    @property
+    def get_balance(self):
+        return self.__balance
 
-        
+    @get_balance.setter
+    def get_balance(self, new_balance):
+        if isinstance(new_balance, (int, float)):
+            raise ValueError("Balance must be a number")
+        self.__balance = new_balance
+
+    def deposit(self, amount):
+        if isinstance(amount, (int, float)):
+            raise ValueError("Amount must be a number")
+
+        if amount < 0:
+            raise ValueError("Amount must be a greater than zero ")
+
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if isinstance(amount, (int, float)):
+            raise ValueError("Amount must be a number")
+
+        if amount < 0 or self.__balance < 0:
+            raise ValueError(
+                "Withdraw Amount must be a greater than zero  or balance is insufficient"
+            )
+
+        self.__balance -= amount
