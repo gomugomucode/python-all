e21b85dabc6e4261e808ce9f1e3a8c8a4760a1ea
gomugomucode <alinedtwins@gmail.com>
2026-07-08 21:31:27 +0545

    feat: add numpy array creation and basic operations for data manipulation

---
+++ 1 month python ai ml/numpy_code.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+array = np.array([[1, 2, 3], 
+                  [4, 5, 6], 
+                  [7, 8, 9]])
+
+# Accessing elements
+print(array[0, 0])  
+
+print(array[1, 2])
+
+# Slicing
+print(array[0:2, 1:3])  
