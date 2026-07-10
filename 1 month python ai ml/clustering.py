6184a6d37865500ba9b33c6ca1858a4de04d5176
gomugomucode <alinedtwins@gmail.com>
2026-07-10 19:39:12 +0545

    refactor: remove KMeans example and clean up linear regression code

--- 1 month python ai ml/clustering.py
+++ 1 month python ai ml/clustering.py
@@ -1,14 +1,29 @@
-from sklearn.cluster import KMeans
+# from sklearn.cluster import KMeans
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# X = np.array([[1, 2],
+#               [1, 4],   
+#               [1, 0],
+#                   [4, 2],
+#                   [4, 4],
+#                   [4, 0]])
+
+# model = KMeans(n_clusters=2, random_state=0)
+# model.fit(X)    
+# print(model.labels_)
+
+
+
+
+# underfitting
+from sklearn.linear_model import LinearRegression
 import numpy as np
-import matplotlib.pyplot as plt
 
-X = np.array([[1, 2],
-              [1, 4],   
-              [1, 0],
-                  [4, 2],
-                  [4, 4],
-                  [4, 0]])
+X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+y =np.array([1, 1, 2, 2])
+model = LinearRegression().fit(X, y)
+model.fit(X, y)
+print(model.predict([[1]]))
 
-model = KMeans(n_clusters=2, random_state=0)
-model.fit(X)    
-print(model.labels_)
+
