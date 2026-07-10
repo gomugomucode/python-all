d3a1c7c38294f202ec891c9a5e386ad83672f267
gomugomucode <alinedtwins@gmail.com>
2026-07-10 19:51:46 +0545

    refactor: uncomment KMeans example and clean up code structure

--- 1 month python ai ml/clustering.py
+++ 1 month python ai ml/clustering.py
@@ -1,30 +1,30 @@
-# from sklearn.cluster import KMeans
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# X = np.array([[1, 2],
-#               [1, 4],   
-#               [1, 0],
-#                   [4, 2],
-#                   [4, 4],
-#                   [4, 0]])
-
-# model = KMeans(n_clusters=2, random_state=0)
-# model.fit(X)    
-# print(model.labels_)
-
-
-
-
-# underfitting
-from sklearn.linear_model import LinearRegression
+from sklearn.cluster import KMeans
 import numpy as np
+import matplotlib.pyplot as plt
 
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-y =np.array([1, 1, 2, 2])
-model = LinearRegression().fit(X, y)
-model.fit(X, y)
-print(model.predict([[1]]))
+X = np.array([[1, 2],
+              [1, 4],   
+              [1, 0],
+                  [4, 2],
+                  [4, 4],
+                  [4, 0]])
+
+model = KMeans(n_clusters=2, random_state=0)
+model.fit(X)    
+print(model.labels_)
+
+
+
+
+# # underfitting
+# from sklearn.linear_model import LinearRegression
+# import numpy as np
+
+# X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+# y =np.array([1, 1, 2, 2])
+# model = LinearRegression().fit(X, y)
+# model.fit(X, y)
+# print(model.predict([[1 ]]))
 
 
 
@@ -35,4 +35,4 @@ print(model.predict([[1]]))
 # y = np.array([1, 1, 2, 2])
 # model = DecisionTreeRegressor().fit(X, y)
 # model.fit(X, y)
-# print(model.predict([[1]]))
+# print(model.predict([[1 ]]))
