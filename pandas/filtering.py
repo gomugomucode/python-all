8c72216e8abb5f71e6b2096f805e3e28f0c7b818
gomugomucode <alinedtwins@gmail.com>
2026-07-11 02:09:44 +0545

    refactor: clean up filtering.py by removing commented code and updating print statements

--- pandas/filtering.py
+++ pandas/filtering.py
@@ -23,18 +23,33 @@
 # in thsi the isin tool is used to check the multiple co0lums of the colun type1
 
 
+# import pandas as pd
+
+# df = pd.read_csv('data.csv'  )
+
+# #  CLEANING STEP: Remove all accidental spaces from column names
+# df.columns = df.columns.str.strip()
+
+# # . Double-check our hard work
+# print("Cleaned Columns:", df.columns.tolist())
+
+# # water_poke = df.loc[df['Type 1'].isin(['Water', 'Ice']) ]
+# water_poke =  df[df['Type1'].isin(['Water', 'Ice'])]
+
+# print("Water and Ice Type Pokemon:")
+# print(water_poke)
+
+# usin and (&) operator
+
+
 import pandas as pd
 
 df = pd.read_csv('data.csv'  )
 
-#  CLEANING STEP: Remove all accidental spaces from column names
+# Strip out all the messy leading/trailing spaces from the column headers
 df.columns = df.columns.str.strip()
 
-# . Double-check our hard work
-print("Cleaned Columns:", df.columns.tolist())
+ff_pokemon = df[(df['Type1'] == 'Fire') & (df['Type2'] == 'Flying')]
 
-# water_poke = df.loc[df['Type 1'].isin(['Water', 'Ice']) ]
-water_poke =  df[df['Type1'].isin(['Water', 'Ice'])]
-
-print("Water and Ice Type Pokemon:")
-print(water_poke)
+print("Fire and Flying Type Pokemon:")
+print(ff_pokemon)
