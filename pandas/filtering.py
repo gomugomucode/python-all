1f34684e954ad56f30e5fb11464f3cd348fb02da
gomugomucode <alinedtwins@gmail.com>
2026-07-11 02:05:42 +0545

    feat: update filtering.py to include water and ice type Pokemon filtering and clean column names

--- pandas/filtering.py
+++ pandas/filtering.py
@@ -1,9 +1,40 @@
-import pandas as pd
+# import pandas as pd
 
-df = pd.read_csv('data.csv' , index_col = 'Name')
+# df = pd.read_csv('data.csv' , index_col = 'Name' )
 
 # tall_pokemon = df[df['Height'] > 2.0 ]
 
-tall_pokemon = df.loc[df['Height'] > 2.0 ,["Height"]]
-print("Tall Pokemon:")
-print(tall_pokemon)
+
+# # in this fies the df[height'] > 2.0 is a boolean series which is used to filter the rows of the dataframe where the height is greater than 2.0 and then we are selecting only the height column using [["Height"]]
+# tall_pokemon = df.loc[df['Height'] > 2.0 ,["Height"]]
+# print("Tall Pokemon:")
+# print(tall_pokemon)
+
+
+
+
+# heavy_pokemon = df.loc[df['Weight'] > 100.0 ,["Weight"]]
+# print("Heavy Pokemon:")
+# print(heavy_pokemon)
+
+
+# using the or(|) operator
+
+# in thsi the isin tool is used to check the multiple co0lums of the colun type1
+
+
+import pandas as pd
+
+df = pd.read_csv('data.csv'  )
+
+#  CLEANING STEP: Remove all accidental spaces from column names
+df.columns = df.columns.str.strip()
+
+# . Double-check our hard work
+print("Cleaned Columns:", df.columns.tolist())
+
+# water_poke = df.loc[df['Type 1'].isin(['Water', 'Ice']) ]
+water_poke =  df[df['Type1'].isin(['Water', 'Ice'])]
+
+print("Water and Ice Type Pokemon:")
+print(water_poke)
