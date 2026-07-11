e1a4abddc6e431fb1a7e35b9bff67ad822df1adf
gomugomucode <alinedtwins@gmail.com>
2026-07-11 13:21:38 +0545

    feat: enhance aggregate_function.py with descriptive print statements for numeric calculations

--- pandas/aggregate_function.py
+++ pandas/aggregate_function.py
@@ -10,12 +10,15 @@ df = pd.read_csv('data.csv')
 print(df.mean(numeric_only=True))  #this will print the mean of all the numeric values in the data frame and it will ignore the string values
 
 # like above we can do other aggregate functions like sum, min, max, median, std, var, etc.
+print("sum of all the numeric values in the data frame is: ", df.sum(numeric_only=True))  #this will print the sum of all the numeric values in the data frame and it will ignore the string values``
 
-print(df.sum(numeric_only=True))  #this will print the sum of all the numeric values in the data frame and it will ignore the string values
-print(df.min(numeric_only=True))  #this will print the minimum of all the numeric values in the data frame and it will ignore the string values
-print(df.max(numeric_only=True))  #this will print the maximum of all the numeric values in the data frame and it will ignore the string values
-print(df.median(numeric_only=True))  #this will print the median of all the numeric values in the data frame and it will ignore the string values
+print("minimum of all the numeric values in the data frame is: ", df.min(numeric_only=True))  #this will print the minimum of all the numeric values in the data frame and it will ignore the string values
 
-print(df.std(numeric_only=True))  #this will print the standard deviation of all the numeric values in the data frame and it will ignore the string values
+print("maximum of all the numeric values in the data frame is: ", df.max(numeric_only=True))  #this will print the maximum of all the numeric values in the data frame and it will ignore the string values
 
-print(df.count(numeric_only=True))  #this will print the count of all the numeric values in the data frame and it will ignore the string values
+print("median of all the numeric values in the data frame is: ", df.median(numeric_only=True))  #this will print the median of all the numeric values in the data frame and it will ignore the string values
+
+
+print("standard deviation of all the numeric values in the data frame is: ", df.std(numeric_only=True))  #this will print the standard deviation of all the numeric values in the data frame and it will ignore the string values
+
+print("count of all the numeric values in the data frame is: ", df.count(numeric_only=True))  #this will print the count of all the numeric values in the data frame and it will ignore the string values
