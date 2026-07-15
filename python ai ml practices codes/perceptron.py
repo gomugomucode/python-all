7acdf1e12712051f91f2d7565a13c6770526a4b8
gomugomucode <alinedtwins@gmail.com>
2026-07-15 22:39:08 +0545

    feat: add weighted sum method to Perceptron class for computation

--- python ai ml practices codes/perceptron.py
+++ python ai ml practices codes/perceptron.py
@@ -8,3 +8,23 @@
 
 # cool_perceptron  = Perceptron()
 # print(cool_perceptron)
+
+
+# now adding the weighted sum variable o store the data
+
+class Perceptron:
+  def __init__(self, num_inputs=2, weights=[2,1]):
+    self.num_inputs = num_inputs
+    self.weights = weights
+    
+    # 
+  def weighted_sum(self, inputs):
+    # create variable to store weighted sum
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      pass
+      # complete this loop
+    return weighted_sum 
+      
+cool_perceptron = Perceptron()
+
