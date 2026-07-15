b53a32f21bc07608e715feb47793f5dfcfa9de24
gomugomucode <alinedtwins@gmail.com>
2026-07-15 19:30:58 +0545

    feat: enhance unsupervised learning script with hierarchical clustering implementation

--- 1 month python ai ml/unsuperizedlearning.py
+++ 1 month python ai ml/unsuperizedlearning.py
@@ -6,3 +6,16 @@ kmeans = KMeans(n_clusters=2 , random_state=42)
 kmeans.fit(X)
 print(kmeans.labels_)
 print(kmeans.cluster_centers_)
+
+
+# hierarchical clustering
+
+from scipy.cluster.hierarchy import dendrogram , linkage
+import matplotlib as plt
+
+Z = linkage(X , method='ward')
+dendrogram(Z)
+plt.show()
+
+
+# 
