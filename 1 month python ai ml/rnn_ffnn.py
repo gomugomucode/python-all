c2191bb176a7ec6165589ac7ec102889f8e9aa11
gomugomucode <alinedtwins@gmail.com>
2026-07-23 19:18:45 +0545

    feat: add comparison documentation for RNN and FFNN architectures

---
+++ 1 month python ai ml/rnn_ffnn.py
@@ -0,0 +1,20 @@
+# RNN  = RECURRENT  NEURAL NETWORK
+
+# FFNN = FEED-FORWARD NEURAL NETWORK
+
+
+# RNN  use the internal feedback loop that create memory  where the FFNN passes the information strctly forward in one direction
+
+
+# FFNN  data flow
+# input -> Hidden layer -> output
+
+
+# RNN data flow (cyclic)
+# Input -> Hidden -> output -> Next Input
+
+
+# Key Difference
+
+# FFNN  :  no memory
+# RNN   :  have memory
