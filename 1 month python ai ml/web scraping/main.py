319056fdb00a98777aa7394087a8e5bb48bb4d22
gomugomucode <alinedtwins@gmail.com>
2026-07-26 19:37:19 +0545

    feat: add web scraping script for COVID-19 data and configure Python interpreter path

--- 1 month python ai ml/web scraping/main.py
+++ 1 month python ai ml/web scraping/main.py
@@ -1,7 +1,7 @@
-from urllib3 import response
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
 
 url = "https://www.worldometers.info/coronavirus/"
 
@@ -9,4 +9,15 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-print(soup)
+# print(soup)
+
+table = soup.find("table", {"id": "main_table_countries_today"})
+
+# print(table)
+header = []
+
+if table:
+    for th in table.find_all("th"):
+        header.append(th.text.strip())
+
+print(header)
