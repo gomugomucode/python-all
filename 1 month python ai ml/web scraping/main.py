f2bfd3a78d67ea4d622a97fd34674859923f30e8
gomugomucode <alinedtwins@gmail.com>
2026-07-26 19:41:21 +0545

    feat: add web scraper to fetch and save worldometers coronavirus data to csv

--- 1 month python ai ml/web scraping/main.py
+++ 1 month python ai ml/web scraping/main.py
@@ -20,4 +20,18 @@ if table:
     for th in table.find_all("th"):
         header.append(th.text.strip())
 
-print(header)
+# print(header)
+
+row = []
+
+for tr in table.find_all("tr")[1:]:
+    td = tr.find_all("td")
+    row.append([td[i].text.strip() for i in range(len(td))])
+
+# print(row)
+
+df = pd.DataFrame(row, columns=header)
+print(df)
+
+df.to_csv("world_ometers_corona_data.csv", index=False)
+print("data saved to world_ometers_corona_data.csv")
