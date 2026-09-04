253383dada34d7ad26afde247dd8c3a2d21609ef
gomugomucode <alinedtwins@gmail.com>
2026-09-04 22:16:13 +0545

    feat: add initial example for basic line plotting with labels in matplotlib

---
+++ mathplot/label.py
@@ -0,0 +1,18 @@
+# # With Pyplot, you can use the xlabel() and ylabel() functions to set a label for the x- and y-axis.
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# xarray = np.array([1, 2, 3, 4, 5])
+# yarray = np.array([2, 4, 6, 8, 10])
+
+# plt.plot(xarray, yarray, marker='o', linestyle='--', color='g', label='y = 2x')
+
+# # With Pyplot, you can use the title() function to set a title for the plot.
+# plt.title('Line Plot Example')
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+
+# plt.show()
+
+
