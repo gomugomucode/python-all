facf9a04320b45eae41c8543ebbebfb945a579e1
gomugomucode <alinedtwins@gmail.com>
2026-09-04 07:57:55 +0545

    feat: add start.py with basic matplotlib plotting examples

--- mathplot/start.py
+++ mathplot/start.py
@@ -1,74 +1,88 @@
-# import matplotlib
+# # import matplotlib
 
-# print(matplotlib.__version__)
+# # print(matplotlib.__version__)
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+
+
+# # fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+# # ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
+# # plt.show()                           # Show the figure.
+
+
+# # fig = plt.figure()             # an empty figure with no Axes
+# # fig, ax = plt.subplots()       # a figure with a single Axes
+# # fig, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
+# # # a figure with one Axes on the left, and two on the right:
+# # fig, axs = plt.subplot_mosaic([['left', 'right_top'],
+# #                                ['left', 'right_bottom']])
+
+# # plt.show()
+
+# # np.random.seed(19680801)  # seed the random number generator.
+# # data = {'a': np.arange(50),
+# #         'c': np.random.randint(0, 50, 50),
+# #         'd': np.random.randn(50)}
+# # data['b'] = data['a'] + 10 * np.random.randn(50)
+# # data['d'] = np.abs(data['d']) * 100
+
+# # fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+# # ax.scatter('a', 'b', c='c', s='d', data=data)
+# # ax.set_xlabel('entry a')
+# # ax.set_ylabel('entry b')
+# # plt.show()
+
+
+
+
+
+# # matplotlib  pyplot
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# # creating the numpy array containing the horizontal and verticla points
+
+# xpoints = np.array([0, 6])
+# ypoints = np.array([0, 250])
+
+# # plotting the points using the plot()
+
+# # plot function is used to draw the line between two points 
+
+# # plt.plot(xpoints, ypoints)   
+
+# # if u  want to draw the axis not the line in the graph or plot then pass 'o' in the plot function  
+
+# # Draws only circles at (0, 0) and (6, 250) without a connecting line
+# plt.plot(xpoints, ypoints, 'o')
+
+
+
+# # if u Want both the line and the dots in the graph or plot then pass 'o' in the plot function  
+# # You can combine a marker 'o' with a line '-' to get both:
+
+# # Draws both the dots and the line connecting them
+# # plt.plot(xpoints, ypoints, marker='o')
+
+# # If you want to change the appearance of the line and the marker, you can use the fmt argument:
+# plt.plot(xpoints, ypoints, 'o:r')
+
+# plt.show()
+
+
+
+# plotting multiple points in a graph or plot
+
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
 
-
-# fig, ax = plt.subplots()             # Create a figure containing a single Axes.
-# ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
-# plt.show()                           # Show the figure.
-
-
-# fig = plt.figure()             # an empty figure with no Axes
-# fig, ax = plt.subplots()       # a figure with a single Axes
-# fig, axs = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
-# # a figure with one Axes on the left, and two on the right:
-# fig, axs = plt.subplot_mosaic([['left', 'right_top'],
-#                                ['left', 'right_bottom']])
-
-# plt.show()
-
-# np.random.seed(19680801)  # seed the random number generator.
-# data = {'a': np.arange(50),
-#         'c': np.random.randint(0, 50, 50),
-#         'd': np.random.randn(50)}
-# data['b'] = data['a'] + 10 * np.random.randn(50)
-# data['d'] = np.abs(data['d']) * 100
-
-# fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
-# ax.scatter('a', 'b', c='c', s='d', data=data)
-# ax.set_xlabel('entry a')
-# ax.set_ylabel('entry b')
-# plt.show()
-
-
-
-
-
-# matplotlib  pyplot
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-# creating the numpy array containing the horizontal and verticla points
-
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-
-# plotting the points using the plot()
-
-# plot function is used to draw the line between two points 
-
-# plt.plot(xpoints, ypoints)   
-
-# if u  want to draw the axis not the line in the graph or plot then pass 'o' in the plot function  
-
-# Draws only circles at (0, 0) and (6, 250) without a connecting line
-plt.plot(xpoints, ypoints, 'o')
-
-
-
-# if u Want both the line and the dots in the graph or plot then pass 'o' in the plot function  
-# You can combine a marker 'o' with a line '-' to get both:
-
-# Draws both the dots and the line connecting them
-# plt.plot(xpoints, ypoints, marker='o')
-
-# If you want to change the appearance of the line and the marker, you can use the fmt argument:
-plt.plot(xpoints, ypoints, 'o:r')
-
+plt.plot(xpoints, ypoints)
 plt.show()
