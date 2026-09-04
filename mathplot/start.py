774797f0439607cecb68e654cd35a4cba0111dd9
gomugomucode <alinedtwins@gmail.com>
2026-09-04 21:45:03 +0545

    feat: add example for plotting multiple lines in a single graph

--- mathplot/start.py
+++ mathplot/start.py
@@ -164,14 +164,29 @@
 
 #  we ca also create multiple lines in a single graph or plot
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# y1 = np.array([3, 8, 1, 10])
+# y2 = np.array([6, 2, 7, 11])
+
+# plt.plot(y1)
+# plt.plot(y2)
+
+# plt.show()
+
+
+
+#  drawing the multiple line in the same plot using the single plot function
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+x1 = np.array([0, 1, 2, 3])
 y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
 y2 = np.array([6, 2, 7, 11])
 
-plt.plot(y1)
-plt.plot(y2)
-
+plt.plot(x1, y1, x2, y2)
 plt.show()
 
