f3aa5c2cfda006defcbb567663501f40127d97cf
gomugomucode <alinedtwins@gmail.com>
2026-09-04 22:42:57 +0545

    feat: add examples for using grid lines and title positioning in matplotlib

--- mathplot/label.py
+++ mathplot/label.py
@@ -33,3 +33,71 @@
 
 # plt.plot(x, y)
 # plt.show()
+
+
+
+# We can use the loc parameter in title() to position the title.
+
+# Legal values are: 'left', 'right', and 'center'. Default value is 'center'.
+
+# Title spacing: Uses pad  
+# Label spacing (X and Y): Uses labelpad
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.title("Sports Watch Data", loc = 'right' , pad = 20)
+# plt.xlabel("Average Pulse", loc = 'center', labelpad = 20)
+# plt.ylabel("Calorie Burnage", loc = 'bottom', labelpad = 20)
+
+# plt.plot(x, y)
+# plt.show()
+
+
+
+
+# With Pyplot, we  can use the grid() function to add grid lines to the plot.
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+
+# plt.plot(x, y)
+
+# plt.grid()
+
+# plt.show() 
+
+
+
+
+
+
+# we can use the axis parameter in the grid() function to specify which grid lines to display.
+
+# Legal values are: 'x', 'y', and 'both'. Default value is 'both'.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis = 'y')
+
+plt.show() 
