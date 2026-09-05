904a80405ec46ac8d628d288d1920d432e5296cc
gomugomucode <alinedtwins@gmail.com>
2026-09-05 20:09:27 +0545

    feat: add examples for setting bar colors and width in bar chart

--- mathplot/bar.py
+++ mathplot/bar.py
@@ -22,3 +22,34 @@
 
 # plt.barh(x, y)
 # plt.show()
+
+
+# we can also use the color argument to set the color of the bars:
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
+
+# # plt.bar(x, y, color = "red")
+
+# #  we can alos use the hexadecimal color code to set the color of the bars:
+
+
+# plt.bar(x, y, color = "#4CAF50")
+
+# plt.show()
+
+
+
+#  we can also specify the width of the bars by using the width argument:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, width = 0.1 , color = "#FAF200")
+plt.show()
