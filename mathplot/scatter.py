987284f0430b8124ba223f4548c2eb6482ddef88
gomugomucode <alinedtwins@gmail.com>
2026-09-05 19:59:07 +0545

    feat: update scatter plot example to demonstrate individual point coloring with colormap

--- mathplot/scatter.py
+++ mathplot/scatter.py
@@ -43,15 +43,28 @@
 # plt.show() 
 
 
-#  we can also color each  and every point individually by using the c argument and specifying a list of colors:
+# #  we can also color each  and every point individually by using the c argument and specifying a list of colors:
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+
+# plt.scatter(x, y, c=colors)
+
+# plt.show() 
+
+# we can create the color array and specify the colormap in the scatter() method, using the cmap argument:
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
 y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+colors = np.array([0, 100, 20, 6, 40, 45, 50, 55, 60, 70, 80, 90, 100])
 
-plt.scatter(x, y, c=colors)
+plt.scatter(x, y, c=colors, cmap='viridis')
 
 plt.show() 
