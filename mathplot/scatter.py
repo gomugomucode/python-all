efb596d8a246ffa7b5f6cd35f7b9ee9eaaba642c
gomugomucode <alinedtwins@gmail.com>
2026-09-05 19:57:23 +0545

    feat: add example for individual point coloring in scatter plot

--- mathplot/scatter.py
+++ mathplot/scatter.py
@@ -41,3 +41,17 @@
 # plt.scatter(x, y, color = '#88c999')
 
 # plt.show() 
+
+
+#  we can also color each  and every point individually by using the c argument and specifying a list of colors:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+
+plt.scatter(x, y, c=colors)
+
+plt.show() 
