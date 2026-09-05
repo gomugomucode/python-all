0c33779929b8ed2bd6ee0c8fbc4bda9c8ddcc190
gomugomucode <alinedtwins@gmail.com>
2026-09-05 19:52:28 +0545

    refactor: clean up commented-out code and add titles to subplots in matplotlib example

--- mathplot/subplot.py
+++ mathplot/subplot.py
@@ -1,3 +1,30 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# #plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+
+# # plt.subplot(rows, columns, position)
+# plt.subplot(1, 2, 1)
+# plt.plot(x,y)
+
+
+# #plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(1, 2, 2)
+# plt.plot(x,y)
+
+# plt.show()
+
+
+
+
+# multiple plot with seperate title
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -5,11 +32,9 @@ import numpy as np
 x = np.array([0, 1, 2, 3])
 y = np.array([3, 8, 1, 10])
 
-
-# plt.subplot(rows, columns, position)
 plt.subplot(1, 2, 1)
 plt.plot(x,y)
-
+plt.title("SALES")
 
 #plot 2:
 x = np.array([0, 1, 2, 3])
@@ -17,5 +42,6 @@ y = np.array([10, 20, 30, 40])
 
 plt.subplot(1, 2, 2)
 plt.plot(x,y)
+plt.title("INCOME")
 
 plt.show()
