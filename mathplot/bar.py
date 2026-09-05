1c3f6b4295fb771cea72b9f66264eed79c93bd42
gomugomucode <alinedtwins@gmail.com>
2026-09-05 20:06:57 +0545

    feat: comment out bar chart example code in bar.py

--- mathplot/bar.py
+++ mathplot/bar.py
@@ -1,12 +1,24 @@
 
 
-#  we can create bar fuctions to create the bar chart 
+# #  we can create bar fuctions to create the bar chart 
 
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-x = np.array(["A", "B", "C", "D"])
-y = np.array([3, 8, 1, 10])
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
 
-plt.bar(x,y)
-plt.show()
+# plt.bar(x,y)
+# plt.show()
+
+
+# we can also create vertically
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.array(["A", "B", "C", "D"])
+# y = np.array([3, 8, 1, 10])
+
+# plt.barh(x, y)
+# plt.show()
