5a2719a0d67ca9d7726e52649c7a46f8c95a586c
gomugomucode <alinedtwins@gmail.com>
2026-09-05 20:15:02 +0545

    feat: add initial histogram example using matplotlib

---
+++ mathplot/histogram.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# 1. Your data list
+data = [1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 6, 7, 8, 9]
+
+# 2. Make the histogram
+plt.hist(data)
+
+# 3. Show it on screen
+plt.show()
