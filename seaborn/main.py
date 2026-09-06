ffe5a5b1b5177a7ae4e593a996942e02d77b0045
gomugomucode <alinedtwins@gmail.com>
2026-09-06 06:28:20 +0545

    feat: add seaborn example for visualizing tips dataset with scatter plot

---
+++ seaborn/main.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# sns.defaullt_theme()
+
+#  here the  tips is the dataset which is used to plot the graph
+# #  it has  data like this 
+
+#      total_bill   tip     sex smoker   day    time  size
+# 0         16.99  1.01  Female     No   Sun  Dinner     2
+# 1         10.34  1.66    Male     No   Sun  Dinner     3
+# 2         21.01  3.50    Male     No   Sun  Dinner     3
+# 3         23.68  3.31    Male     No   Sun  Dinner     2
+# 4         24.59  3.61  Female     No   Sun  Dinner     4
+
+tips = sns.load_dataset("tips")
+
+sns.relplot(data = tips, x = "total_bill", y = "tip")
+
+# plt.show()
+
+print(tips)
